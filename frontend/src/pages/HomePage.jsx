c975a45dae18cead1198916dad7e1670485b8e62
DavidIncarbone <david.incarbone@gmail.com>
2025-04-07 14:24:25 +0200

    test1

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -3,22 +3,22 @@ import { useStae, useEffect } from "react";
 
 export default function HomePage() {
 
-    const apiURL = import.meta.env.VITE_API_URL;
-    const endpoint = "posts";
+    // const apiURL = import.meta.env.VITE_API_URL;
+    // const endpoint = "posts";
 
-    useEffect(() => { fetchPosts(), [] });
+    // useEffect(() => { fetchPosts(), [] });
 
-    const fetchPosts = () => {
-        axios.get(apiURL + endpoint).then((res) => {
-            console.log(res.data);
-        }).catch((err) => {
+    // const fetchPosts = () => {
+    //     axios.get(apiURL + endpoint).then((res) => {
+    //         console.log(res.data);
+    //     }).catch((err) => {
 
-            console.log(err);
+    //         console.log(err);
 
-        }).finally(() => {
-            console.log("Chiamata effettuata");
-        });
-    };
+    //     }).finally(() => {
+    //         console.log("Chiamata effettuata");
+    //     });
+    // };
     return (
         <div>
             <h1>HomePage</h1>
