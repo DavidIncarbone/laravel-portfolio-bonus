7ee60e4a17a4d782a2e0a318fdb8745bab4eb521
DavidIncarbone <david.incarbone@gmail.com>
2025-04-10 12:52:52 +0200

    fixed login and logout and denie permissions for guests

--- backend/resources/views/partials/sidebar.blade.php
+++ backend/resources/views/partials/sidebar.blade.php
@@ -1,13 +1,18 @@
 <!-- Sidebar -->
 <nav class="sidebar bg-dark text-white p-4">
-    <h3 class="text-center">Admin Dashboard</h3>
+
     <ul class="nav flex-column">
+        <li>
+            <a href="{{ route('admin.home') }}" class="text-decoration-none">
+                <h3 class="text-center">Admin Dashboard</h3>
+            </a>
+        </li>
         <li class="nav-item">
             <a href="http://localhost:5174" class="nav-link text-white">Vai al Portfolio</a>
         </li>
-        <li class="nav-item">
+        {{-- <li class="nav-item">
             <a href="{{ route('admin.profile.index') }}" class="nav-link text-white">Gestisci Profilo</a>
-        </li>
+        </li> --}}
         <li class="nav-item">
             <a href="{{ route('admin.projects.index') }}" class="nav-link text-white">Gestisci i Progetti</a>
         </li>
@@ -17,16 +22,18 @@
         <li class="nav-item">
             <a href="{{ route('admin.technologies.index') }}" class="nav-link text-white">Gestisci tecnologie</a>
         </li>
-        <li class="nav-item">
+        @auth
+            <li class="nav-item">
 
-            <a class="dropdown-item" href="{{ route('logout') }}"
-                onclick="event.preventDefault();
+                <a class="nav-link text-white" href="{{ route('logout') }}"
+                    onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
-                {{ __('Logout') }}
-            </a>
-            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                @csrf
-            </form>
-        </li>
+                    {{ __('Logout') }}
+                </a>
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                    @csrf
+                </form>
+            </li>
+        @endauth
     </ul>
 </nav>
--- backend/routes/web.php
+++ backend/routes/web.php
@@ -6,14 +6,18 @@ use App\Http\Controllers\Admin\TypeController;
 use App\Http\Controllers\Admin\TechnologyController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('admin');
+
+Route::get("/", function () {
+    return redirect()->route("admin.home");
 });
 
 Route::middleware(["auth", "verified"])
     ->name("admin.")
     ->prefix("admin")
     ->group(function () {
+        Route::get('/', function () {
+            return view('admin');
+        })->name("home");
         Route::resource("profile", ProfileController::class);
         Route::resource("projects", ProjectController::class);
         Route::resource("types", TypeController::class);
